a9452c025362857617345bf5246454e13a13bf18
kibs86 <kibs086@gmail.com>
2016-11-17 13:26:24 -0500

    add doctor_id to patient and has_many/belongs_to
    - add doctor_id to patient table
    - add has_many and belongs_to to models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117154703) do
+ActiveRecord::Schema.define(version: 20161117181438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,10 @@ ActiveRecord::Schema.define(version: 20161117154703) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "secret_info"
+    t.integer  "doctor_id"
   end
+
+  add_index "patients", ["doctor_id"], name: "index_patients_on_doctor_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
@@ -54,4 +57,5 @@ ActiveRecord::Schema.define(version: 20161117154703) do
   add_index "users", ["token"], name: "index_users_on_token", unique: true, using: :btree
 
   add_foreign_key "examples", "users"
+  add_foreign_key "patients", "doctors"
 end
