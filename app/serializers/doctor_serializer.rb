b6369a44ff52924b32d5959a61b256239cec3f2d
kibs86 <kibs086@gmail.com>
2016-11-22 14:00:55 -0500

    update serializers and create appointments
    - update doctor and patient serializers to show appointments
    - create appointments through curl and rails console

--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -1,4 +1,4 @@
 class DoctorSerializer < ActiveModel::Serializer
-  attributes :id, :given_name, :family_name
+  attributes :id, :given_name, :family_name, :appointments
   # has_many :patients
 end
--- app/serializers/patient_serializer.rb
+++ app/serializers/patient_serializer.rb
@@ -1,4 +1,4 @@
 class PatientSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :sickness
+  attributes :id, :name, :age, :sickness, :appointments
   # belongs_to :doctor
 end
